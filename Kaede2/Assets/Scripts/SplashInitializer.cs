0058e8094a1ab3c212bfd26b72b65cc3d1b2cda9
Runxuan Shu <shurunxuan@hotmail.com>
2024-02-24 22:14:56 +0800

    added copy assetbundles to streamingassets

--- Kaede2/Assets/Scripts/SplashInitializer.cs
+++ Kaede2/Assets/Scripts/SplashInitializer.cs
@@ -3,67 +3,31 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using Kaede2.Assets.AssetBundles;
-using Kaede2.Assets.ScriptableObjects;
 using Kaede2.Utils;
+using UnityEngine.Serialization;
 
 namespace Kaede2
 {
     public class SplashInitializer : MonoBehaviour
     {
         [SerializeField]
-        private List<Image> _splashSprites;
+        private List<Image> splashSprites;
 
         [SerializeField]
-        private float _delayDuration = 0.5f;
+        private float delayDuration = 0.5f;
 
         [SerializeField]
-        private float _fadeDuration = 0.5f;
+        private float fadeDuration = 0.5f;
 
         [SerializeField]
-        private float _splashDuration = 2.0f;
+        private float splashDuration = 2.0f;
 
         private void Awake()
         {
-            foreach (var image in _splashSprites)
+            foreach (var image in splashSprites)
             {
                 image.color = new Color(1, 1, 1, 0);
             }
-        }
-
-        private void SetSplashSpritesColor(Color c)
-        {
-            foreach (var image in _splashSprites)
-            {
-                image.color = c;
-            }
-        }
-
-        private IEnumerator SplashColor()
-        {
-            yield return new WaitForSeconds(_delayDuration);
-
-            float currentTime = Time.time;
-
-            while (Time.time - currentTime < _fadeDuration)
-            {
-                SetSplashSpritesColor(new Color(1, 1, 1, Mathf.Clamp01((Time.time - currentTime) / _fadeDuration)));
-                yield return null;
-            }
-            
-            SetSplashSpritesColor(new Color(1, 1, 1, 1));
-
-            yield return new WaitForSeconds(_splashDuration);
-
-            currentTime = Time.time;
-            
-            while (Time.time - currentTime < _fadeDuration)
-            {
-                SetSplashSpritesColor(new Color(1, 1, 1, Mathf.Clamp01(1 - (Time.time - currentTime) / _fadeDuration)));
-
-                yield return null;
-            }
-
-            SetSplashSpritesColor(new Color(1, 1, 1, 0));
         }
 
         private IEnumerator Start()
@@ -72,31 +36,46 @@ namespace Kaede2
             coroutineGroup.Add(SplashColor(), this);
             coroutineGroup.Add(AssetBundleManifestData.LoadManifest(), this);
             yield return coroutineGroup.WaitForAll();
+        }
 
-            // for testing only
-            ResourceLoader loader = new ResourceLoader();
-            var loadLoopInfoRequest = loader.LoadRequest<AudioLoopInfo>("audio/bgm/yu3_BGM_Adv01_Final.loopinfo");
-            var loadAudioClipRequest = loader.LoadRequest<AudioClip>("audio/bgm/yu3_BGM_Adv01_Final.wav");
-
-            loadLoopInfoRequest.onProgressCallback = f => Debug.Log($"Loading Loop Info progress: {f * 100}%");
-            loadAudioClipRequest.onProgressCallback = f => Debug.Log($"Loading Audio Clip progress: {f * 100}%");
-
-            loadLoopInfoRequest.onFinishedCallback = info =>
+        private void SetSplashSpritesColor(Color c)
+        {
+            foreach (var image in splashSprites)
             {
-                Debug.Log($"Loop start: {info.loop_info[0].start}, Loop end: {info.loop_info[0].end}");
-            };
+                image.color = c;
+            }
+        }
 
-            CoroutineGroup loadAudioGroup = new CoroutineGroup();
-            loadAudioGroup.Add(loadLoopInfoRequest.Send(), this);
-            loadAudioGroup.Add(loadAudioClipRequest.Send(), this);
-            yield return loadAudioGroup.WaitForAll();
+        private IEnumerator SplashColor()
+        {
+            yield return new WaitForSeconds(delayDuration);
 
-            GameObject newObj = new GameObject();
-            var source = newObj.AddComponent<AudioSource>();
-            source.clip = loadAudioClipRequest.Result;
-            source.loop = true;
-            source.spatialize = false;
-            source.Play();
+            float currentTime = Time.time;
+
+            while (Time.time - currentTime < fadeDuration)
+            {
+                var a = Mathf.Clamp01((Time.time - currentTime) / fadeDuration);
+                a = Mathf.Pow(a, 4);
+                SetSplashSpritesColor(new Color(1, 1, 1, a));
+                yield return null;
+            }
+            
+            SetSplashSpritesColor(new Color(1, 1, 1, 1));
+
+            yield return new WaitForSeconds(splashDuration);
+
+            currentTime = Time.time;
+            
+            while (Time.time - currentTime < fadeDuration)
+            {
+                var a = Mathf.Clamp01(1 - (Time.time - currentTime) / fadeDuration);
+                a = Mathf.Pow(a, 4);
+                SetSplashSpritesColor(new Color(1, 1, 1, a));
+
+                yield return null;
+            }
+
+            SetSplashSpritesColor(new Color(1, 1, 1, 0));
         }
     }
 }
