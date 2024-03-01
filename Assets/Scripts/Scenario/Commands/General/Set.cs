f843df05a34122c71555e44e4a9063d5795ce1c2
Runxuan Shu <runxuan.shu@unity.cn>
2024-03-01 17:52:42 +0800

    added wait and set commands

---
+++ Assets/Scripts/Scenario/Commands/General/Set.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+
+namespace Kaede2.Scenario.Commands
+{
+    public class Set : ScenarioModule.Command
+    {
+        private readonly string variable;
+        private readonly string value;
+
+        public Set(ScenarioModule module, string[] arguments) : base(module, arguments)
+        {
+            string[] split = OriginalArg(1).Split('=');
+            variable = split[0].Trim();
+            value = split[1].Trim();
+        }
+
+        public override ExecutionType Type => ExecutionType.Instant;
+        public override float ExpectedExecutionTime => 0;
+
+        public override IEnumerator Execute()
+        {
+            Module.AddVariable(variable, value);
+            yield break;
+        }
+
+        public override void Undo()
+        {
+            // it's okay to not remove the variable, because it will be overwritten
+        }
+    }
+}
