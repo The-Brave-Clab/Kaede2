06bbb34df417390fcdd27beb3382901ecb7bda89
Runxuan Shu <shurunxuan@hotmail.com>
2024-03-02 09:51:07 +0800

    added loading canvas

---
+++ Assets/Scripts/UI/ScenarioScene/UIManager.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using Kaede2.Utils;
+using UnityEngine;
+
+namespace Kaede2.UI.ScenarioScene
+{
+    public class UIManager : Singleton<UIManager>
+    {
+        public Canvas loadingCanvas;
+    }
+}
