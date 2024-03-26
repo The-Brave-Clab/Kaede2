850674fd67e9841872c7e773c4f3f62f6de15bb9
Runxuan Shu <shurunxuan@hotmail.com>
2024-03-26 22:47:31 +0800

    fixed masterdata importer

--- Assets/_Kaede2Assets/Scripts/Editor/Importers/MasterDataImporter.cs
+++ Assets/_Kaede2Assets/Scripts/Editor/Importers/MasterDataImporter.cs
@@ -1,45 +1,41 @@
 using System;
-using System.Collections.Generic;
 using System.IO;
 using UnityEditor.AssetImporters;
 using UnityEngine;
-using Kaede2.ScriptableObjects;
 
 namespace Kaede2.Editor.Importers
 {
     [ScriptedImporter(1, "masterdata")]
     public class MasterDataImporter : ScriptedImporter
     {
-        static List<Type> masterDataTypes = null;
-
         public override void OnImportAsset(AssetImportContext ctx)
         {
-            if (masterDataTypes == null)
+            if (ctx.assetPath == null) return;
+            var assetFileName = Path.GetFileNameWithoutExtension(ctx.assetPath);
+
+            Type masterDataType = FindType(assetFileName);
+            if (masterDataType == null) return;
+
+            var text = File.ReadAllText(ctx.assetPath);
+            var data = ScriptableObject.CreateInstance(masterDataType);
+            JsonUtility.FromJsonOverwrite(text, data);
+            ctx.AddObjectToAsset(ctx.assetPath, data);
+        }
+
+        private static Type FindType(string name)
+        {
+            foreach (var assembly in AppDomain.CurrentDomain.GetAssemblies())
             {
-                masterDataTypes = new List<Type>();
-                foreach (var assembly in AppDomain.CurrentDomain.GetAssemblies())
+                foreach (var type in assembly.GetTypes())
                 {
-                    foreach (var type in assembly.GetTypes())
+                    if (type.Name == name)
                     {
-                        if (type.IsSubclassOf(typeof(BaseMasterData<>)))
-                        {
-                            masterDataTypes.Add(type);
-                        }
+                        return type;
                     }
                 }
             }
-            
 
-            if (ctx.assetPath == null) return;
-            var assetFileName = Path.GetFileNameWithoutExtension(ctx.assetPath);
-            if (masterDataTypes.Exists(t => t.Name == assetFileName))
-            {
-                var text = File.ReadAllText(ctx.assetPath);
-                var type = masterDataTypes.Find(t => t.Name == assetFileName);
-                var data = ScriptableObject.CreateInstance(type);
-                JsonUtility.FromJsonOverwrite(text, data);
-                ctx.AddObjectToAsset(ctx.assetPath, data);
-            }
+            return null;
         }
     }
 }
