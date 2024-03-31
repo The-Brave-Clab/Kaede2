81ef7e1b6cb862ccb472b22ea25f2549cee4807c
Runxuan Shu <shurunxuan@hotmail.com>
2024-03-31 21:47:24 +0800

    saving textures on windows

--- Assets/_Kaede2Assets/Scripts/Utils/SaveTexture.cs
+++ Assets/_Kaede2Assets/Scripts/Utils/SaveTexture.cs
@@ -54,7 +54,14 @@ namespace Kaede2.Utils
             typeof(SaveTexture).Log($"Saving texture to {name}...");
 
 #if UNITY_STANDALONE_WIN || UNITY_EDITOR_WIN
-            throw new System.NotImplementedException();
+            string filePath = OpenWinSaveFileDialog("Save Texture", directory, name);
+            if (filePath != null)
+            {
+                if (Path.GetExtension(filePath) == "")
+                    filePath += ".png";
+                File.WriteAllBytes(filePath, bytes);
+                directory = Path.GetDirectoryName(filePath);
+            }
 #elif UNITY_STANDALONE_OSX || UNITY_EDITOR_OSX
             pngData = bytes;
             IntPtr titlePtr = Marshal.StringToHGlobalAuto("Save Texture");
@@ -79,9 +86,74 @@ namespace Kaede2.Utils
 #endif
         }
 
+#if UNITY_STANDALONE_WIN || UNITY_EDITOR_WIN
+        [DllImport("comdlg32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+        private static extern bool GetSaveFileName([In, Out] OPENFILENAME ofn);
+
+        [DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+        private static extern IntPtr GetActiveWindow();
+
+        [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Auto)]
+        private struct OPENFILENAME
+        {
+            public int lStructSize;
+            public IntPtr hwndOwner;
+            public IntPtr hInstance;
+            public string lpstrFilter;
+            public string lpstrCustomFilter;
+            public int nMaxCustFilter;
+            public int nFilterIndex;
+            public IntPtr lpstrFile;
+            public int nMaxFile;
+            public string lpstrFileTitle;
+            public int nMaxFileTitle;
+            public string lpstrInitialDir;
+            public string lpstrTitle;
+            public int Flags;
+            public short nFileOffset;
+            public short nFileExtension;
+            public string lpstrDefExt;
+            public IntPtr lCustData;
+            public IntPtr lpfnHook;
+            public string lpTemplateName;
+            public IntPtr pvReserved;
+            public int dwReserved;
+            public int FlagsEx;
+        }
+
+        private static string OpenWinSaveFileDialog(string title, string directory, string defaultFileName)
+        {
+            IntPtr buffer = Marshal.StringToHGlobalAuto(new string('\0', 1024));
+            // copy default file name to buffer
+            Marshal.Copy(defaultFileName.ToCharArray(), 0, buffer, defaultFileName.Length);
+
+            OPENFILENAME ofn = new OPENFILENAME();
+            ofn.lStructSize = Marshal.SizeOf(ofn);
+            ofn.hwndOwner = GetActiveWindow();
+            ofn.lpstrFile = buffer;
+            ofn.lpstrInitialDir = directory;
+            ofn.lpstrTitle = title;
+            ofn.Flags = 0x00000002 | 0x00000800 | 0x00001000 | 0x00000008; // OFN_OVERWRITEPROMPT | OFN_PATHMUSTEXIST | OFN_HIDEREADONLY | OFN_NOCHANGEDIR
+            ofn.nMaxFile = 1024;
+            ofn.lpstrFilter = "PNG Files\0*.png\0";
+
+            if (GetSaveFileName(ofn))
+            {
+                string filePath = Marshal.PtrToStringAuto(ofn.lpstrFile);
+                typeof(SaveTexture).Log("Selected file path: " + filePath);
+                return filePath;
+            }
+            else
+            {
+                typeof(SaveTexture).Log("No file was selected or dialog was cancelled.");
+            }
+
+            return null;
+        }
+#endif
 #if UNITY_STANDALONE_OSX || UNITY_EDITOR_OSX
         [DllImport("MacSaveFileDialog")]
-        static extern void OpenMacSaveFileDialog(IntPtr title, IntPtr directory, IntPtr filename, Action<IntPtr> callback);
+        private static extern void OpenMacSaveFileDialog(IntPtr title, IntPtr directory, IntPtr filename, Action<IntPtr> callback);
 
         [MonoPInvokeCallback(typeof(Action<IntPtr>))]
         private static void OnFileSelected(IntPtr filePathPtr)
