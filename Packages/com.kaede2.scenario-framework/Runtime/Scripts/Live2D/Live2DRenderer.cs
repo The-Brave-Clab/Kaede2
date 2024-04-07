4e4f58e830779ebc00ff82700c681b88a846de64
Runxuan Shu <runxuan.shu@unity.cn>
2024-04-07 09:53:26 +0800

    use unity time for live2d time

--- Packages/com.kaede2.scenario-framework/Runtime/Scripts/Live2D/Live2DRenderer.cs
+++ Packages/com.kaede2.scenario-framework/Runtime/Scripts/Live2D/Live2DRenderer.cs
@@ -1,11 +1,15 @@
-﻿using System;
-using Kaede2.Scenario.Framework.Entities;
+﻿using Kaede2.Scenario.Framework.Entities;
 using UnityEngine;
 
 namespace Kaede2.Scenario.Framework.Live2D
 {
     public class Live2DRenderer : MonoBehaviour
     {
+        private void Update()
+        {
+            live2d.UtSystem.setUserTimeMSec((long)(Time.timeAsDouble * 1000));
+        }
+
         private void OnPostRender()
         {
             Live2DActorEntity.AllActors.ForEach(a => a.Render());
