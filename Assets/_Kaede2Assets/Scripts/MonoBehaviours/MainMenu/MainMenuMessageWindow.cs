37bf692cbb702bb12210e7351ecb097ebed3f39b
Runxuan Shu <runxuan.shu@unity.cn>
2024-04-13 23:11:21 +0800

    added rich text to main menu message window

--- Assets/_Kaede2Assets/Scripts/MonoBehaviours/MainMenu/MainMenuMessageWindow.cs
+++ Assets/_Kaede2Assets/Scripts/MonoBehaviours/MainMenu/MainMenuMessageWindow.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using DG.Tweening;
+using Kaede2.Scenario.Framework.UI;
 using TMPro;
 using UnityEngine;
 
@@ -14,7 +15,7 @@ namespace Kaede2
 
         private Coroutine changeTextCoroutine;
 
-        private string currentText;
+        private RichText currentText;
 
         private void Awake()
         {
@@ -23,7 +24,7 @@ namespace Kaede2
 
         public void ChangeText(string newText)
         {
-            if (string.Equals(currentText, newText))
+            if (string.Equals(currentText.MacroText, newText))
                 return;
 
             currentText = newText;
@@ -41,14 +42,14 @@ namespace Kaede2
                 // we need to have the text laid out before starting the typewriter effect
                 // so we embrace the not-shown text with color tag <color=#FFF0></color>
 
-                string shownPart = currentText[..index];
-                string hiddenPart = currentText[index..];
+                string shownPart = currentText.Substring(0, index);
+                string hiddenPart = currentText.Substring(index, currentText.Length - index);
                 text.text = $"{shownPart}<color=#FFF0>{hiddenPart}</color>";
                 yield return new WaitForSeconds(0.01f);
                 index++;
             }
 
-            text.text = currentText;
+            text.text = currentText.ToString();
         }
     }
 }
