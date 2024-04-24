dee15ef17d6102ac950ed7ccb8578acd9d49a3fd
Runxuan Shu <runxuan.shu@unity.cn>
2024-04-24 22:52:33 +0800

    improved full screen canvas and added safe area support

---
+++ Assets/_Kaede2Assets/Scripts/UI/FullscreenCanvas.cs
@@ -0,0 +1,78 @@
+using UnityEngine;
+
+namespace Kaede2.UI
+{
+    [RequireComponent(typeof(RectTransform))]
+    [RequireComponent(typeof(Canvas))]
+    [ExecuteAlways]
+    public class FullscreenCanvas : MonoBehaviour
+    {
+        protected Canvas canvas;
+        protected RectTransform rectTransform;
+        protected Canvas rootCanvas;
+        protected RectTransform rootCanvasRT;
+
+        protected bool driving;
+
+        [SerializeField]
+        private bool safeArea = false;
+
+        private void Awake()
+        {
+            driving = false;
+            rectTransform = GetComponent<RectTransform>();
+            canvas = GetComponent<Canvas>();
+
+            rootCanvas = canvas.rootCanvas;
+            rootCanvasRT = rootCanvas.GetComponent<RectTransform>();
+
+            if (canvas != null && rectTransform != null && rootCanvas != null && rootCanvasRT != null && !canvas.isRootCanvas && rectTransform.drivenByObject == null)
+            {
+                DrivenRectTransformTracker tracker = new();
+                tracker.Clear();
+                tracker.Add(this, rectTransform, DrivenTransformProperties.All);
+                driving = true;
+            }
+        }
+
+        protected virtual void LateUpdate()
+        {
+            if (!driving) return;
+
+            // get accumulated scale and negate it
+            Vector3 scale = Vector3.one;
+            Transform parent = transform.parent;
+            while (parent != rootCanvasRT)
+            {
+                scale = Vector3.Scale(scale, parent.localScale);
+                parent = parent.parent;
+            }
+            var newScale = new Vector3(1 / scale.x, 1 / scale.y, 1 / scale.z);
+            if (float.IsFinite(newScale.x) && float.IsFinite(newScale.y) && float.IsFinite(newScale.z))
+            {
+                rectTransform.localScale = newScale;
+            }
+
+            rectTransform.rotation = rootCanvasRT.rotation;
+
+            rectTransform.pivot = rootCanvasRT.pivot;
+            rectTransform.position = rootCanvasRT.position;
+            if (safeArea)
+            {
+                var safeAreaRect = Screen.safeArea;
+                Vector2 resolution = new Vector2(Screen.width, Screen.height);
+                var offset = safeAreaRect.center - resolution / 2.0f;
+                var rootScale = rootCanvasRT.localScale;
+                rectTransform.position += new Vector3(offset.x, offset.y);
+                rectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, safeAreaRect.width / rootScale.x);
+                rectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, safeAreaRect.height / rootScale.y);
+            }
+            else
+            {
+                var rootRect = rootCanvasRT.rect;
+                rectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, rootRect.width);
+                rectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, rootRect.height);
+            }
+        }
+    }
+}
