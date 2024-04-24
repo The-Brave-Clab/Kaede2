b19e47381cd8178290b698ca5e14295a2aa87302
Runxuan Shu <runxuan.shu@unity.cn>
2024-04-24 23:02:38 +0800

    fixed tagging problem

--- Assets/_Kaede2Assets/Scripts/ScriptableObjects/MasterData/MasterAlbumInfo.cs
+++ Assets/_Kaede2Assets/Scripts/ScriptableObjects/MasterData/MasterAlbumInfo.cs
@@ -26,18 +26,21 @@ namespace Kaede2.ScriptableObjects
         // we have 1999 illustrations, with 40 bundles, each bundle will contain 50 items
         // the split here is caused by main menu scene using one of the illustrations and has a very long loading time
         private const int BundleCount = 40;
-        private AlbumInfo[] sortedAlbumInfo = null;
         public static int GetBundleIndex(string albumName)
         {
             // find the index inside master data
             int illustIndex = -1;
-            // we are going to display the illust in order of name, so sorting the bundle helps reducing the loading time and memory usage
-            Instance.sortedAlbumInfo ??= Instance.albumInfo.OrderBy(i => i.AlbumName).ToArray();
-            for (int i = 0; i < Instance.sortedAlbumInfo.Length; i++)
+            // we are going to display the illust in order of name, so sorting the bundle helps to reduce the loading time and memory usage
+            int i = 0;
+            foreach (var info in Instance.albumInfo.OrderBy(ai => ai.AlbumName))
             {
-                if (!string.Equals(Instance.sortedAlbumInfo[i].AlbumName, albumName)) continue;
-                illustIndex = i;
-                break;
+                if (string.Equals(info.AlbumName, albumName))
+                {
+                    illustIndex = i;
+                    break;
+                }
+
+                ++i;
             }
 
             if (illustIndex == -1)
@@ -46,7 +49,7 @@ namespace Kaede2.ScriptableObjects
                 return -1;
             }
 
-            var bundleIndex = illustIndex / Mathf.RoundToInt((float)Instance.sortedAlbumInfo.Length / BundleCount);
+            var bundleIndex = illustIndex / Mathf.RoundToInt((float)Instance.albumInfo.Length / BundleCount);
             return bundleIndex;
         }
     }
